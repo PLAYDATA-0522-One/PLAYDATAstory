1508e555c3146bc9913872bf7bcf270b4a0d3f14
Amy <wlgpek90@naver.com>
2023-06-09 19:51:41 +0900

    230609

--- src/config/JdbcConnection.java
+++ src/config/JdbcConnection.java
@@ -5,10 +5,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class JdbcConnection {
-    private final String url = "jdbc:mysql://localhost:3306/test2" +
+    private final String url = "jdbc:mysql://127.0.0.1:3306/PlaydataStory" +
             "?serverTimezone=Asia/Seoul&characterEncoding=UTF-8";
-    private final String root = "root";
-    private final String password = "1q2w3e4r!!";
+    private final String root = "PlaydataStory";
+    private final String password = "1q2w3e4r";
 
     public Connection getJdbc() {
         Connection conn;
--- src/repository/UserRepository.java
+++ src/repository/UserRepository.java
@@ -11,18 +11,18 @@ import java.sql.SQLException;
 public class UserRepository {
     public boolean login(String id, String password)  {
         Connection conn = new JdbcConnection().getJdbc();
-        String sql = "select * from (데이터테이블이름) where username = ? and password = ?";
-        Integer id1 = null;
-        String name = null;
+        String sql = "select * from user_id where username = ? and password = ?";
+        String username = null;
+        String job = null;
         try {
             PreparedStatement psmt = conn.prepareStatement(sql);
             psmt.setString(1,id);
             psmt.setString(2,password);
             ResultSet resultSet = psmt.executeQuery();
             while (resultSet.next()) {
-                id1 = resultSet.getInt("id");
-                name = resultSet.getString("username");
-                System.out.println(id1 +" "+name+"환영해요");
+                username = resultSet.getString("username");
+                job = resultSet.getString("job");
+                System.out.println(username +" "+job+" 환영해요");
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -32,12 +32,12 @@ public class UserRepository {
         } catch (SQLException e) {
             System.out.println("conn 닫기 실패");;
         }
-        return id1 != null;
+        return username != null;
     }
 
     public void insertUsers(SignupDto dto) {
         Connection conn = new JdbcConnection().getJdbc();
-        String sql ="insert into (데이터테이블이름)(username, password, job)\n" +
+        String sql ="insert into user_id(username, password, job)\n" +
                 "value (?, ?, ?);";
         try {
             PreparedStatement psmt = conn.prepareStatement(sql);
