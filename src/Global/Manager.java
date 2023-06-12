70023e83e7cf58b17af426c93b97589744f6cc60
gotoAbraxas <lty7085@gmail.com>
2023-06-12 09:52:28 +0900

    0612 9

--- src/Global/Manager.java
+++ src/Global/Manager.java
@@ -2,20 +2,22 @@ package Global;
 
 import Map.GameMap;
 import character.Human;
+import character.Humaninfo;
+import character.Monster;
 
 public class Manager { //extends thread{
     public static Human human = null;
     public static GameMap map;
-
+    public static Monster monster;
+    public static Humaninfo humaninfo;
+    // history
     private static int fullHp;
 
     public void saveHp() {
         if (human != null) {
             fullHp = human.getHp();
         }
-
     }
-
     public int getFullHp() {
         return fullHp;
     }
---
+++ src/character/Humaninfo.java
@@ -0,0 +1,66 @@
+package character;
+
+public class Humaninfo{
+
+    int id;
+    String job;
+    String name;
+    int hp;
+    int Attackpoint;
+
+    public Humaninfo(int id, String job, String name, int hp, int attackpoint) {
+        this.id = id;
+        this.job = job;
+        this.name = name;
+        this.hp = hp;
+        Attackpoint = attackpoint;
+    }
+
+
+    //    public void setStatus(Human human){
+//        this.setHp(human.getHp());
+//        this.setAttackpoint(human.getAttackpoint());
+//
+//    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getAttackpoint() {
+        return Attackpoint;
+    }
+
+    public void setAttackpoint(int attackpoint) {
+        Attackpoint = attackpoint;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
